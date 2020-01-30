758315c1f75c8a748e2849ed05d614c94cf88ba7
shivagomasa <shivagomasa@gmail.com>
2020-01-30 18:38:45 +0530

    Create a Registration model.

--- matrimony_app/models.py
+++ matrimony_app/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.base_user import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.utils.translation import ugettext_lazy as _
 from django.utils import timezone
+from django.conf import settings
 
 from .managers import UserManager
 # Create your models here.
@@ -29,4 +30,41 @@ class User(AbstractBaseUser,PermissionsMixin):
     def get_full_name(self):
         return '%s %s' % (self.first_name,self.last_name)
 
+
+class Registration(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    PROFILE_CHOICES = [
+        ('Myself','Myself'),
+        ('Son','Son'),
+        ('Daughter','Daughter'),
+        ('Brother','Brother'),
+        ('Sister','Sister'),
+        ('Friend','Friend'),
+        ('Relative','Relative'),
+    ]
+    profile_for = models.CharField(choices=PROFILE_CHOICES,max_length=10)
+    GENDER_CHOICES = [
+        ('Male','Male'),
+        ('Female','Female'),
+    ]
+    gender = models.CharField(choices=GENDER_CHOICES,max_length=10)
+    RELIGION_CHOICES = [
+        ('Hindu','Hindu'),
+        ('Muslim','Muslim'),
+        ('Christian','Christian'),
+        ('Sikh','Sikh'),
+        ('Buddist','Buddist'),
+    ]
+    religion = models.CharField(choices=RELIGION_CHOICES,max_length=10)
+    MOTHER_TONGUE_CHOICES = [
+        ('Hindi','Hindi'),
+        ('English','English'),
+        ('Marathi','Marathi'),
+        ('Telugu','Telugu'),
+        ('Tamil','Tamil'),
+    ]
+    mother_tongue = models.CharField(choices=MOTHER_TONGUE_CHOICES,max_length=10)
+
+    def __str__(self):
+        return self.user
 
