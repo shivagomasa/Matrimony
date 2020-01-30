4efc4949ae91dfea1b215aa198c3ed291ab5351d
shivagomasa <shivagomasa@gmail.com>
2020-01-30 18:16:09 +0530

    Register the user model in admin interface and add some filters.

--- matrimony_app/admin.py
+++ matrimony_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import User
 
 # Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display =('first_name','last_name','email','phone_number','registration_date',)
+    search_fields = ('first_name','email')
+    list_filter = ('first_name','email',)
--- matrimony_app/models.py
+++ matrimony_app/models.py
@@ -28,3 +28,5 @@ class User(AbstractBaseUser,PermissionsMixin):
 
     def get_full_name(self):
         return '%s %s' % (self.first_name,self.last_name)
+
+
